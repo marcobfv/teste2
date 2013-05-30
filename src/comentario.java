d49f925da83f75a08216191aee2fd4b50862e572
Marco <marco.bfv@gmail.com>
2013-05-30 12:53:34 -0300

    Usando JSTL MANO

--- src/comentario.java
+++ src/comentario.java
@@ -1,12 +1,14 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet(name="comentario", urlPatterns="/coisa-coiso")
 public class comentario extends HttpServlet{
 
 	@Override
@@ -17,9 +19,21 @@ public class comentario extends HttpServlet{
 		saida.println("<html>");
 		saida.println("<title>Servlet</title>");
 		saida.println("<body>");
-		saida.println("Hello Mr. Dog");
+		saida.println("Hello Mr. auau");
 		saida.println("</body>");
 		saida.println("</html>");
 		
+	}
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
 	}
 }
